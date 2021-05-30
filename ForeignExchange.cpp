486dc1c1e40d9043bb63c11ccce449da2aac6fe6
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2021-05-30 06:38:48 +0300

    Update ForeignExchange.cpp

--- ForeignExchange.cpp
+++ ForeignExchange.cpp
@@ -1,4 +1,4 @@
-
+//DFS
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -56,6 +56,68 @@ int main()
                               happy = false;
                               break;
                         }
+                  }
+                  if (happy)
+                  {
+                        cout << "YES" << endl;
+                  }
+                  else
+                  {
+                        cout << "NO" << endl;
+                  }
+            }
+      }
+}
+//Implementation
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+const lld pi = 3.14159265358979323846;
+const ll mod = 1000000007;
+const ll template_array_size = 1e6 + 332001;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+#define send                                  \
+      {                                       \
+            ios_base::sync_with_stdio(false); \
+      }
+#define help               \
+      {                    \
+            cin.tie(NULL); \
+      }
+int main()
+{
+      //freopen("equal.in", "r", stdin);
+      send help ll n;
+      while (true)
+      {
+            cin >> n;
+            if (n == 0)
+            {
+                  break;
+            }
+            else
+            {
+                  ll a, b, x = 1, y = 1;
+                  bool happy = true;
+                  if (n % 2 != 0)
+                        happy = false;
+                  vector<vector<int>> tie(500000 + 10);
+                  while (n--)
+                  {
+
+                        cin >> a >> b;
+                        x *= a;
+                        y *= b;
+                  }
+                  // cout << x << " " << y;
+                  if (x != y)
+                  {
+                        happy = false;
                   }
                   if (happy)
                   {
