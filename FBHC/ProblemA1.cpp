dff63a85eaaf3e2622d670289639e3890d6ce59e
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2021-08-28 13:20:46 -0700

    Update ProblemA1.cpp

--- FBHC/ProblemA1.cpp
+++ FBHC/ProblemA1.cpp
@@ -1,4 +1,3 @@
-https://www.facebook.com/codingcompetitions/hacker-cup/2021/qualification-round/problems/A1\
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -7,21 +6,68 @@ typedef long double lld;
 typedef unsigned long long ull;
 const lld pi = 3.14159265358979323846;
 const ll MOD = 1000000007;
+//"A", "E", "I", "O", and "U"
+bool isV(char c) {
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+        return true;
+    }
+    return false;
+}
 
 signed main() {
-    freopen("fuel.in", "r", stdin);
-    ll tt, k, a, b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll tt;
+    string str;
     cin >> tt;
-    while (tt--) {
-        cin >> k >> a >> b;
-        if (a == b || (a > k && b > k)) {
-            cout << 0 << "\n";
-        } else if (a + b == k) {
-            cout << abs(a - b);
+    for (int i = 1; i <= tt; ++i) {
+        cin >> str;
+        ll n = str.size();
+        if (n == 1) {
+            cout << "Case #" << i << ": " << 0 << "\n";
         } else {
-            lld mn = min(a, b);
-            ll rest = k - mn;
-            cout << rest - mn;
+            int vow[26];
+            int con[26];
+            ll cv = 0, cc = 0;
+            memset(vow, 0, sizeof vow);
+            memset(con, 0, sizeof con);
+            for (int i = 0; i < n; ++i) {
+                if (isV(str[i])) {
+                    cv++;
+                    vow[(ll) str[i] - 'A']++;
+                    //cout << (ll) str[i] - 'A';
+                } else {
+                    cc++;
+                    con[(ll) str[i] - 'A']++;
+
+                }
+            }
+            ll index = 0, mx = 0, ans = 0;
+            if (cc >= cv) {
+                ans += cc;
+                ans += cv * 2;
+                for (int i = 0; i < 26; ++i) {
+                    if (vow[i] > mx) {
+                        mx = vow[i];
+                        index = i;
+                    }
+                }
+                ll done = vow[index];
+                ans -= done * 2;
+            } else {
+                ans += cv;
+                ans += cc * 2;
+                for (int i = 0; i < 26; ++i) {
+                    if (con[i] > mx) {
+                        mx = con[i];
+                        index = i;
+                    }
+                }
+                ll done = con[index];
+                ans -= done * 2;
+            }
+            cout << "Case #" << i << ": " << ans << "\n";
         }
     }
 }
@@ -33,4 +79,3 @@ signed main() {
 
 
 
-v
