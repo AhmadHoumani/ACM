3a2002d1d6eb8d74f8ccafb87cf27c4692368d60
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2021-08-28 06:54:38 -0700

    Update ProblemB.cpp

--- FBHC/ProblemB.cpp
+++ FBHC/ProblemB.cpp
@@ -1,4 +1,3 @@
-//https://www.facebook.com/codingcompetitions/hacker-cup/2021/qualification-round/problems/B
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -9,39 +8,64 @@ const lld pi = 3.14159265358979323846;
 const ll MOD = 1000000007;
 
 signed main() {
+
     int tt, n;
     cin >> tt;
     while (tt--) {
         cin >> n;
         char a[n][n];
-        int rows[n];
-        fill(rows, rows + n, n);
+        int row[n];
         int col[n];
+        fill(row, row + n, n);
         fill(col, col + n, n);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 cin >> a[i][j];
                 if (a[i][j] == 'O') {
-                    rows[j] = col[i] = n + 1;
-
+                    row[i] = n + 1;
+                    continue;
                 } else if (a[i][j] == 'X') {
-                    rows[j]--, col[j]--;
+                    row[i]--;
                 }
             }
         }
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                cout << a[i][j] << " ";
+                if (a[j][i] == 'O') {
+                    col[i] = n + 1;
+                    continue;
+                } else if (a[j][i] == 'X') {
+                    col[i]--;
+                }
             }
-            cout << "\n";
         }
-        cout << "\n";
-        for (auto x: rows) {
-            cout << x << " ";
+        ll mnr = n + 1;
+        ll mnc = n + 1;
+
+        for (int i = 0; i < n; i++) {
+            if (row[i] < mnr) {
+                mnr = row[i];
+            }
+            if (col[i] < mnc) {
+                mnc = col[i];
+            }
         }
-        cout << "\n";
-        for (auto x: col) {
-            cout << x << " ";
+        ll r = 0;
+        ll c = 0;
+        for (int i = 0; i < n; ++i) {
+            if (row[i] == mnr) {
+                r++;
+            }
+            if (col[i] == mnc) {
+                c++;
+            }
+        }
+        if (mnc == mnr) {
+            cout << mnr << " " << c + r << "\n";
+        } else if (mnc > mnr) {
+            cout << mnr << " " << r << "\n";
+        } else {
+            cout << mnc << " " << c << "\n";
         }
     }
 }
