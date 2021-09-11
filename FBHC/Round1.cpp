8695906cbeacb1e55f66cc9c01ad007e37752710
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2021-09-11 14:19:32 -0700

    Create Round1.cpp

---
+++ FBHC/Round1.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include <fstream>
+
+using namespace std;
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+const lld pi = 3.14159265358979323846;
+const ll MOD = 1000000007;
+
+signed main() {
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cout.tie(0);
+    freopen("Output.txt","w",stdout);
+//    ll tt, n;
+//    string str;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; ++i) {
+//        cin >> n >> str;
+//        char is = 'F';
+//        ll count = 0;
+//        for (char c: str) {
+//            if (c == 'O' || c == 'X') {
+//                is = c;
+//                break;
+//            }
+//        }
+//        if (is == 'F') {
+//            cout << "Case #" << i << ": " << 0 << "\n";
+//        } else {
+//            for (char c: str) {
+//                if (is != c && c != 'F') {
+//                    count++, is = c;
+//                }
+//            }
+//            cout << "Case #" << i << ": " << count << "\n";
+//        }
+//    }
+        cout<<"aa";
+}
+
+
+
+
+
+
+
+
