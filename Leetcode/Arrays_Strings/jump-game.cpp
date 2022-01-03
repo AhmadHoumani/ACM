92ce05ba0b0d792d27d95342f3bc11392f134092
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2022-01-03 09:51:14 -0800

    Create jump-game.cpp

---
+++ Leetcode/Arrays_Strings/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   bool canJump(vector<int> &nums) {
+    int maxReach = 0, n = nums.size();
+    for (int i = 0; i < n; ++i) {
+		if(i>maxReach){
+			return false;
+		}
+		maxReach=max(maxReach,nums[i]+i);
+    }
+    return true;
+}
+};
