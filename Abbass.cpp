b657673bcd5bec8087092ab5360f7a3b78d4054d
Ahmad Houmani <31387896+AhmadHoumani@users.noreply.github.com>
2022-02-24 20:18:33 +0200

    Create Abbass.cpp

---
+++ Abbass.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+const ll MOD = 1000000007;
+#define print(v)     \
+	for (auto x : v) \
+	cout << x << " "
+#define io                            \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL)
+
+struct contributer
+{
+	string name;
+	map<string, int> mp;
+};
+
+struct project
+{
+	string name;
+	int duration;
+	int score;
+	int bestscore;
+	vector<int> con;
+};
+
+int main()
+{
+	//    io;
+	
+}
